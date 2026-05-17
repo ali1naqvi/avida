4d17de4161867041753ba6cb0441db0c9e186ebf
Ali Naqvi <70173682+ali1naqvi@users.noreply.github.com>
2026-05-17 03:11:31 -0400

    update on script

--- scripts_ALI/run_ecology_tests.sh
+++ scripts_ALI/run_ecology_tests.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=avida-ecology
-#SBATCH --time=06:00:00
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=4G
+#SBATCH --account=def-skelly
+
+#SBATCH --ntasks=21               
+#SBATCH --mem-per-cpu=15G      
+#SBATCH --time=0-5:00  # time (DD-HH:MM)
+
 #SBATCH --output=ecology_%j.out
 #SBATCH --error=ecology_%j.err
 
