4e4a6a52a005d368ead8d37929743f24e0b020b4
Ali Naqvi <70173682+ali1naqvi@users.noreply.github.com>
2026-05-17 03:21:40 -0400

    20 runs per test

--- scripts_ALI/run_ecology_tests.sh
+++ scripts_ALI/run_ecology_tests.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #SBATCH --account=def-skelly
-
-#SBATCH --ntasks=1               
-#SBATCH --mem-per-cpu=5G      
-#SBATCH --time=0-5:00  # time (DD-HH:MM)
-
-#SBATCH --output=ecology_%j.out
-#SBATCH --error=ecology_%j.err
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=15G
+#SBATCH --time=0-5:00
+#SBATCH --array=1-20
+#SBATCH --output=ecology_%x_%A_%a.out
+#SBATCH --error=ecology_%x_%A_%a.err
 
 #SBATCH --mail-user=alinaqvi8014@gmail.com
 #SBATCH --mail-type=END,FAIL
@@ -22,5 +21,13 @@ if [ -z "$TEST_NAME" ]; then
   exit 1
 fi
 
-cd ~/scratch/avida/cbuild/work/$TEST_NAME
-../avida -c avida.cfg
+BASE_DIR=~/scratch/avida/cbuild/work
+RUN_DIR=$BASE_DIR/${TEST_NAME}_run_${SLURM_ARRAY_TASK_ID}
+
+rm -rf "$RUN_DIR"
+cp -a "$BASE_DIR/$TEST_NAME" "$RUN_DIR"
+
+cd "$RUN_DIR"
+
+SEED=$SLURM_ARRAY_TASK_ID
+../avida -c avida.cfg -s "$SEED"
